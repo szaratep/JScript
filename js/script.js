e678ed9d5d8b91bde48a8510086d2e0eb7c534cd
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-03-25 20:16:12 -0500

    se crea el convertidor a edad canina

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*
+let nombre = prompt('Dime tu nombre');
+let precio = Number(prompt('Dime el precio de la compra'));
+let cantidadDescuento = Number(prompt('de cuanto quieres el descuento?'));
+let descuento = precio * (cantidadDescuento / 100);
+precio -= descuento;
+console.log(`tu precio final es: ${precio}`);
+
+//si es menor a 100 +10
+//si es de 100 - 200 -10
+//si es mayor a 200 -20
+
+if (precio < 0) {
+    console.log('Precio Invalido');
+} else if (precio == 0) {
+    console.log('te invito a comprar algo ya que aun no tienes precio');
+} else if (precio < 100) {
+    console.log(precio += precio * 0.1);
+} else if (precio >= 100 && precio < 200) {
+    console.log(precio -= precio * 0.1);
+} else {
+    console.log(precio -= precio * 0.2);
+};
+*/
+
+
+alert('hola, puedes convertir de km a m y viceversa');
+let tipo = prompt('Que quieres convertir');
+
+if (tipo == ''){
+    console.log('Necesito una unidad de medida valida');
+} else if(tipo == 'km'){
+    let valor = Number(prompt('Dame tu valor en m: '));
+    if(valor < 0){
+        console.log('necesito un valor valido');
+    }else{
+        console.log('tu valor en m es:', valor *= 1000);
+    }
+}else{
+    let valor = Number(prompt('Dame tu valor en m: '));
+    if(valor < 0){
+        console.log('necesito un valor valido');
+    }else{
+        console.log('tu valor en km es:', valor /= 1000);
+    }
+    
+};
+
+
+
---
+++ js/taller.js
@@ -0,0 +1,7 @@
+//POR FAVOR COMENTAR Y DESCMENTAR CADA VEZ QUE SE UTILICE O SE PRUEBNE EL EJERCICO
+
+// - convertidor a edad canina
+let edad = Number(prompt('Dame la edad'));
+let edadPerruna = 7;
+
+console.log(`La edad de tu perro en años humanos ${edad * edadPerruna}`)
