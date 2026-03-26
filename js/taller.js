74060e715986fb2ba428e37eeae2c984590190a3
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-03-25 20:25:26 -0500

    se crea el convertidor de pesos a dolares

--- js/taller.js
+++ js/taller.js
@@ -3,5 +3,17 @@
 // - convertidor a edad canina
 let edad = Number(prompt('Dame la edad'));
 let edadPerruna = 7;
+console.log(`La edad de tu edad canina es: ${edad * edadPerruna}`);
 
-console.log(`La edad de tu perro en años humanos ${edad * edadPerruna}`)
+// - convertidor de pesos a dolares
+let dolar = Number(prompt('cuantos dolares quieres cambiar?'));
+const tasa = 3693.19;
+alert('la tasa de cambio a dia de hoy es: 3,693.19 COP');
+
+console.log(`el total en COP es: ${dolar*tasa}`)
+
+// - convertidor de grados celcius a farenheit
+let cel = Number(prompt('Dame los grados celcius'));
+let result = (cel *(9/5)) + 32;
+
+console.log(`los grados farenheit son: ${result}`)
