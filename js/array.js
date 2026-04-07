06cb553c0df418b28e0ae1ac7007a01f99bb99f3
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-06 19:50:23 -0500

    promedio

--- js/array.js
+++ js/array.js
@@ -1,3 +1,6 @@
+//productos
+
+/*
 let productos = []
 
 let cont = 0;
@@ -11,3 +14,21 @@ while(cont < cant){
 
 
 console.log(productos);
+*/
+
+//promedio
+
+let notas = [];
+let cont = 0;
+let prom = 0;
+let cant = Number(prompt("¿Cuantas notas quieres agregar?"));
+
+while(cont < cant){
+    cont++
+    let pro = Number(prompt(`Dame la nota #${cont}`));
+    notas.push(pro);
+    prom = prom + pro;
+}
+
+console.log(notas);
+console.log(prom/notas.length);
