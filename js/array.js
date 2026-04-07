2f84605861ef8087b26cdde5ffe9cd01b2d870ef
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-06 19:24:13 -0500

    array

---
+++ js/array.js
@@ -0,0 +1,11 @@
+let productos = []
+
+let pro1 = prompt("Que producto quieres agregar #1?");
+let pro2 = prompt("Que producto quieres agregar #2?");
+let pro3 = prompt("Que producto quieres agregar #3?");
+
+productos.push(pro1);
+productos.push(pro2);
+productos.push(pro3);
+
+console.log(productos);
