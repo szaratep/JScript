3273276d7d72e6ed2e2a0b7db1cd4ac85121da06
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-08 21:40:16 -0500

    estudiantes con descuentos y semestres

--- js/objetos.js
+++ js/objetos.js
@@ -2,35 +2,47 @@ const estudiantes = [
   {
     nombre: "Juan",
     apellido: "Pérez",
-    notas: [4.5, 3.8, 4.2, 4.0]
+    notas: [4.5, 3.8, 4.2, 4.0],
+    semestre: 3,
+    precioSemestre: 2500000
   },
   {
     nombre: "María",
     apellido: "Gómez",
-    notas: [3.5, 3.8, 3.9, 4.1]
+    notas: [3.5, 3.8, 3.9, 4.1],
+    semestre: 5,
+    precioSemestre: 2800000
   },
   {
     nombre: "Carlos",
     apellido: "Rodríguez",
-    notas: [2.5, 2.8, 3.0, 2.9]
+    notas: [2.5, 2.8, 3.0, 2.9],
+    semestre: 2,
+    precioSemestre: 2300000
   },
   {
     nombre: "Laura",
     apellido: "Martínez",
-    notas: [4.8, 4.9, 5.0, 4.7]
+    notas: [4.8, 4.9, 5.0, 4.7],
+    semestre: 7,
+    precioSemestre: 3000000
   },
   {
     nombre: "Andrés",
     apellido: "López",
-    notas: [3.0, 3.2, 3.5, 3.3]
+    notas: [3.0, 3.2, 3.5, 3.3],
+    semestre: 1,
+    precioSemestre: 2200000
   }
 ];
 let suma = 0;
 let promedio = 0;
+let aumento = 0;
 
 for(let i = 0; i < estudiantes.length; i++){
     let name = estudiantes[i].nombre + ' ' +estudiantes[i].apellido;
     suma = 0;
+    aumento = 0;
 
     for(let j = 0; j < estudiantes[i].notas.length; j++){  
         suma += estudiantes[i].notas[j];
@@ -39,10 +51,15 @@ for(let i = 0; i < estudiantes.length; i++){
     promedio = suma / estudiantes[i].notas.length
 
     if (promedio < 3){
+        aumento = estudiantes[i].precioSemestre * 0.2
         console.log(`El estudiante: ${name} perdio con una nota de: ${promedio}`)
+        console.log(`Tiene que repetir el semestre ${estudiantes[i].semestre}, con un valor del 20% adicional, es decir: ${estudiantes[i].precioSemestre + aumento} \n`)
     } else if(promedio <= 5 && promedio >= 3.5){
+        aumento = estudiantes[i].precioSemestre * 0.2
         console.log(`El estudiante: ${name} paso con una promedio de: ${promedio}`)
+        console.log(`Pasa a el semestre ${estudiantes[i].semestre+1}, con un descuento del 20%, es decir: ${estudiantes[i].precioSemestre - aumento} \n`)
     }else{
         console.log(`El estudiante: ${name} tiene que nivelar con una promedio de: ${promedio}`)
+        console.log(`Tiene que nivelar el semestre ${estudiantes[i].semestre}, con un precio de: ${estudiantes[i].precioSemestre} \n`)
     }
 }
