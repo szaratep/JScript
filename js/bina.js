5d03b99c4c8558da1655be89fbdb704d8ef65397
Sebastian Zarate <sebaszarate2005@gmail.com>
2026-04-09 20:12:40 -0500

    correccion al algoritmo binario

--- js/bina.js
+++ js/bina.js
@@ -1,34 +1,25 @@
 //NO COLOCAR MAS DE 500 Y MENOS DE 1
 
 let array = Array.from({ length: 500 }, (_, i) => i + 1);
+let bajo = 0;
+let alto = array.length - 1;
+let number = Number(prompt('Dame un numero del 1 al 500.........'))
+let found = false;
 
-array = array.sort((a,b) => a - b)
-
-console.log(array)
-
-
-
-let number = Number(prompt('Dame el numero a buscar del 1 al 500'));
-let cont = true;
-
-
-while(cont){
-    if(array[0] == number){
-        console.log(`El numero esta en la posicion 0 del arreglo.`);
-        cont = false;
-    }else if(array [array.length-1] == number ){
-        console.log(`El numero esta en la posicion ${array.length - 1} del arreglo.`);
-        cont = false;
-    }
-    
-    let mit = Math.floor(array.length/2)
-    if(array[mit] == number){
-        console.log(`El numero esta en la posicion ${mit} del arreglo.`);
-        cont = false;
-    }else if (number < array [mit]){ 
-        array = array.slice(0, mit);
+while(bajo <= alto){
+    let mit = Math.floor((alto + bajo)/2);
+    if (array[mit] === number){
+        console.log(`El numero esta en el index: ${mit}`);
+        found = true;
+        break;
+    }else if (number < array[mit]){
+        alto = mit - 1;
     }else{
-        array = array.slice(mit, array.length - 1);
+        bajo = mit + 1;
     }
-
 }
+
+if (!found){
+    console.log('numero no encontrado');
+}
+
